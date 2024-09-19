088893cda5cddd0547af88aa178cb16dbaca128c
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-19 12:10:54 +0600

    adding add money event handler (in progress)

--- js/home.js
+++ js/home.js
@@ -1,1 +1,19 @@
-console.log('home added');
+// add money to the account
+
+// step 1 : add a event handler for on clicking the add money button
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // step 2 : get money to be added to the account balance
+    const addMoneyInput = document.getElementById('input-add-money').value;
+
+    console.log(addMoneyInput)
+
+    // get the PIN number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+
+    console.log(pinNumberInput)
+  });
