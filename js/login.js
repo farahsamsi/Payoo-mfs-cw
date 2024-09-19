8d4eb5d65f2a1c5c40d13da1c1d4537f6ef684a1
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-19 11:06:15 +0600

    added login conditions and home page initiated

--- js/login.js
+++ js/login.js
@@ -1,16 +1,26 @@
 // console.log('button clicking file added');
 
-
 // form submit reloading the page
 
-
 // step 1 : set event handler
-document.getElementById('button-login').addEventListener('click', function(event){
+document
+  .getElementById("button-login")
+  .addEventListener("click", function (event) {
     // step 2 : prevent default behavior (prevent reloading browser)
-    event.preventDefault(); 
-    console.log('login button clicked');
+    event.preventDefault();
 
-    // step 3 : get the phone number
-    const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-})
+    // step 3 : get the phone number and pin number
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+
+    console.log(phoneNumber, pinNumber);
+
+    //step 4 : validate the numbers
+    // this temporary, should not do like this
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      console.log("You are logged in");
+      window.location.href = '/home.html'
+    } else {
+      alert("Wrong phone number or PIN");
+    }
+  });
